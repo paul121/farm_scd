fd3d74f6fd97a1ec126948ed48c7a584291a37d8
Paul Weidner <paul.weidner@gmail.com>
2025-07-01 13:53:10 -0700

    Add support for multiple products in herbicide quick form

--- scd_riparian/src/Plugin/QuickForm/Herbicide.php
+++ scd_riparian/src/Plugin/QuickForm/Herbicide.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Drupal\scd_riparian\Plugin\QuickForm;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\ReplaceCommand;
 
 /**
  * @QuickForm(
@@ -68,44 +70,108 @@ class Herbicide extends RiparianMaintenanceBase {
       '#options' => $wind_direction_options,
     ];
 
-    $form['record_data']['product']= [
+    // Products section with dynamic product entries
+    $form['record_data']['products'] = [
       '#type' => 'fieldset',
-      '#title' => $this->t('Product'),
+      '#title' => $this->t('Products'),
+      '#prefix' => '<div id="products-wrapper">',
+      '#suffix' => '</div>',
+      '#tree' => TRUE,
     ];
+
+    // Determine the number of products
+    $num_products = $form_state->get('num_products');
+    $num_products = $num_products ?: 1;
+
+    $triggering_element = $form_state->getTriggeringElement();
+    if ($triggering_element && $triggering_element['#parents'][1] == 'add_product') {
+      $num_products += 1;
+      $form_state->set('num_products', $num_products);
+    }
+    elseif ($triggering_element && $triggering_element['#parents'][1] == 'remove_product') {
+      if ($num_products > 1) {
+        $num_products -= 1;
+      }
+      $form_state->set('num_products', $num_products);
+    }
+
+    // Load material types for dropdown
     $material_types = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree('material_type');
     $material_type_options = [];
     foreach ($material_types as $term) {
       $material_type_options[$term->tid] = $term->name;
     }
-    $form['record_data']['product']['group'] = $this->buildInlineContainer();
-    $form['record_data']['product']['group']['material'] = [
+
+    // Add multiple product entries
+    for ($i = 0; $i < $num_products; $i++) {
+      $form['record_data']['products'][$i] = $this->buildProductEntry($material_type_options, $i);
+    }
+
+    // Add product button
+    $form['record_data']['products']['add_product'] = [
+      '#type' => 'button',
+      '#value' => $this->t('Add Another Product'),
+      '#ajax' => [
+        'callback' => [$this, 'productsCallback'],
+        'wrapper' => 'products-wrapper',
+        'effect' => 'fade',
+      ],
+      '#limit_validation_errors' => [],
+    ];
+    $form['record_data']['products']['remove_product'] = [
+      '#type' => 'button',
+      '#value' => $this->t('Remove Product'),
+      '#ajax' => [
+        'callback' => [$this, 'productsCallback'],
+        'wrapper' => 'products-wrapper',
+        'effect' => 'fade',
+      ],
+      '#limit_validation_errors' => [],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Build a single product entry form.
+   */
+  protected function buildProductEntry(array $material_type_options, int $index) {
+    $product_entry = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Product @count', ['@count' => $index + 1]),
+      '#tree' => TRUE,
+    ];
+
+    $product_entry['material'] = [
       '#type' => 'select',
       '#title' => $this->t('Herbicide product'),
       '#options' => $material_type_options,
+      '#empty_option' => $this->t('- Select -'),
     ];
-    $form['record_data']['product']['group']['total_applied'] = $this->buildQuantityField([
+
+    $product_entry['quantities'] = $this->buildInlineContainer();
+    $product_entry['quantities']['total_applied'] = $this->buildQuantityField([
       'title' => $this->t('Total product applied'),
       'measure' => ['#value' => 'volume'],
       'units' => ['#value' => 'ounces'],
       'value' => ['#min' => 0, '#step' => 0.05],
     ]);
 
-    $form['record_data']['product']['group']['concentration'] = $this->buildQuantityField([
+    $product_entry['quantities']['concentration'] = $this->buildQuantityField([
       'title' => $this->t('Product concentration'),
       'measure' => ['#value' => 'ratio'],
       'units' => ['#value' => 'oz/gal'],
       'value' => ['#min' => 0, '#step' => 0.05],
     ]);
 
-    $form['record_data']['product']['acres'] = $this->buildInlineContainer();
-    $form['record_data']['product']['acres']['acres_treated'] = $this->buildQuantityField([
+    $product_entry['quantities']['acres_treated'] = $this->buildQuantityField([
       'title' => $this->t('Acres treated'),
       'measure' => ['#value' => 'area'],
       'units' => ['#value' => 'acres'],
       'value' => ['#min' => 0],
     ]);
 
-    $form['record_data']['product']['acres']['rate_per_acre'] = $this->buildQuantityField([
+    $product_entry['quantities']['rate_per_acre'] = $this->buildQuantityField([
       'title' => $this->t('Rate per acre'),
       'measure' => ['#value' => 'rate'],
       'units' => ['#options' => [
@@ -114,7 +180,14 @@ class Herbicide extends RiparianMaintenanceBase {
       ]],
       'value' => ['#min' => 0, '#step' => 0.05],
     ]);
-    return $form;
+    return $product_entry;
+  }
+
+  /**
+   * Callback for products.
+   */
+  public function productsCallback(array &$form, FormStateInterface $form_state) {
+    return $form['record_data']['products'];
   }
 
   /**
@@ -132,34 +205,42 @@ class Herbicide extends RiparianMaintenanceBase {
       $old_notes = $log['notes'] ?? '';
       $log['notes'] = "Wind direction: {$form_state->getValue('wind_direction')}\n\n$old_notes";
 
-      // Save material term for material quantities.
-      $material_id = $form_state->getValue('material');
-      $material = $this->entityTypeManager->getStorage('taxonomy_term')->load($material_id);
+      // Process multiple products
+      foreach ($form_state->getValue('products') as $index => $product) {
 
-      // Product quantities.
-      $total_applied = $form_state->getValue('total_applied');
-      $total_applied['type'] = 'material';
-      $total_applied['material_type'] = $material;
-      $log['quantity'][] = $total_applied;
+        // Skip if no material is selected.
+        if (!is_numeric($index) || empty($product['material'])) {
+          continue;
+        }
 
-      $concentration = $form_state->getValue('concentration');
-      $concentration['type'] = 'material';
-      $concentration['material_type'] = $material;
-      $log['quantity'][] = $concentration;
+        // Load material term
+        $material_id = $product['material'];
+        $material = $this->entityTypeManager->getStorage('taxonomy_term')->load($material_id);
 
-      // Acre quantities.
-      $acres_treated = $form_state->getValue('acres_treated');
-      $acres_treated['type'] = 'material';
-      $acres_treated['material_type'] = $material;
-      $log['quantity'][] = $acres_treated;
+        // Product quantities
+        $total_applied = $product['quantities']['total_applied'];
+        $total_applied['type'] = 'material';
+        $total_applied['material_type'] = $material;
+        $log['quantity'][] = $total_applied;
 
-      $rate_per_acre = $form_state->getValue('rate_per_acre');
-      $rate_per_acre['type'] = 'material';
-      $rate_per_acre['material_type'] = $material;
-      $log['quantity'][] = $rate_per_acre;
+        $concentration = $product['quantities']['concentration'];
+        $concentration['type'] = 'material';
+        $concentration['material_type'] = $material;
+        $log['quantity'][] = $concentration;
+
+        // Acre quantities
+        $acres_treated = $product['quantities']['acres_treated'];
+        $acres_treated['type'] = 'material';
+        $acres_treated['material_type'] = $material;
+        $log['quantity'][] = $acres_treated;
+
+        $rate_per_acre = $product['quantities']['rate_per_acre'];
+        $rate_per_acre['type'] = 'material';
+        $rate_per_acre['material_type'] = $material;
+        $log['quantity'][] = $rate_per_acre;
+      }
     }
 
     return $log;
   }
-
 }
